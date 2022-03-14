2c0f8f3cc0c42178e986f4c64cc444f25bc0d435
jiikeeesh <78836181+jiikeeesh@users.noreply.github.com>
2022-03-14 16:26:14 +0545

    abc

--- src/main/java/net/codejava/controller/AppController.java
+++ src/main/java/net/codejava/controller/AppController.java
@@ -1,4 +1,4 @@
-package net.codejava;
+package net.codejava.controller;
 
 import java.io.UnsupportedEncodingException;
 import java.util.List;
@@ -12,6 +12,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import net.codejava.User;
+import net.codejava.UserServices;
 
 @Controller
 public class AppController {
